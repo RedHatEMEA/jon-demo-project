050cec84ed7ad1850dd7ef8074a3c085817097d6
Nabeel Saad <nsaad@redhat.com>
2012-11-18 01:57:57 +0000

    Several improvements on the postgresql side: - fixed the bug after 9.1-4, allowing for install of the latest pgsql - disallowed 9.2 as JON does not support it and doesn't install into it - tidy up of temp files

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -9,6 +9,7 @@ function stopPostgresService () {
 	outputLog "Stopping service..." "2"
 	service $POSTGRES_SERVICE_NAME stop
 }
+
 #function - checkForPostgresOnSystem () - Checks to see if a postgresql instance is installed locally
 function checkForPostgresOnSystem () {
 	
@@ -20,17 +21,21 @@ function checkForPostgresOnSystem () {
 		if [[ "$POSTGRES_SERVICE_FILE" == "" ]]; then
 			outputLog "Postgres service file is not found on the file system." "1"
 			CHECK_SYSTEM=`systemctl | grep postgres`
-				
-			if [[ "$CHECK_SYSTEM" != "" ]]; then
+			CHECK_SYSTEM=${CHECK_SYSTEM%% *}	##Remove anything after the space, if there's an error
+
+			local CHECK_INSTALL=`find /etc/systemd/system/multi-user.target.wants -name "$POSTGRES_SERVICE_NAME"`
+
+			if [[ "$CHECK_SYSTEM" != "" && "$CHECK_INSTALL" != "" ]]; then
 				POSTGRES_SERVICE_NAME=${CHECK_SYSTEM%.service*}
-				
+				outputLog "Service name set to $POSTGRES_SERVICE_NAME"
+								
 				#Set the postgres service file 
 				POSTGRES_SERVICE_FILE=$INIT_D/$POSTGRES_SERVICE_NAME
 			else
 				outputLog "Postgres is not found in the system check." "1"
 				if [[ "$POSTGRES_INSTALLED" != "y" ]]; then 
 					POSTGRES_INSTALLED="n"
-					resetVariableInFile "POSTGRES_INSTALLED" "$POSTGRES_INSTALLED"
+					resetVariableInVariableFile "POSTGRES_INSTALLED" "$POSTGRES_INSTALLED"
 					loadVariables
 				fi
 			fi
@@ -41,8 +46,8 @@ function checkForPostgresOnSystem () {
 			outputLog "POSTGRES_SERVICE_FILE is $POSTGRES_SERVICE_FILE"
 			outputLog "POSTGRES_SERVICE_NAME is $POSTGRES_SERVICE_NAME"
 			
-			resetVariableInFile "POSTGRES_SERVICE_NAME" "${POSTGRES_SERVICE_NAME}"
-			resetVariableInFile "POSTGRES_INSTALLED" "y"
+			resetVariableInVariableFile "POSTGRES_SERVICE_NAME" "${POSTGRES_SERVICE_NAME}"
+			resetVariableInVariableFile "POSTGRES_INSTALLED" "y"
 			
 			if [[ "$POSTGRES_SERVICE_NAME" != "postgresql" ]]; then 
 				VERSION=${POSTGRES_SERVICE_NAME#*-}
@@ -51,13 +56,13 @@ function checkForPostgresOnSystem () {
 				MINOR_VERSION=${VERSION:2:1}
 				outputLog "MAJOR [$MAJOR_VERSION] -- MINOR [$MINOR_VERSION]"
 				
-				resetVariableInFile "POSTGRES_MAJOR_VERSION" "${MAJOR_VERSION}"
-				resetVariableInFile "POSTGRES_MINOR_VERSION" "${MINOR_VERSION}"
+				resetVariableInVariableFile "POSTGRES_MAJOR_VERSION" "${MAJOR_VERSION}"
+				resetVariableInVariableFile "POSTGRES_MINOR_VERSION" "${MINOR_VERSION}"
 			else
 				outputLog "Version of postgresql not found in service name, ignoring version numbers"
 			fi
 			loadVariables
-		fi	
+		fi			
 	fi
 }
 
@@ -98,17 +103,19 @@ function getPostgresRepo () {
 	outputLog "DISTRO_VERSION is $DISTRO_VERSION --  DISTRO is $DISTRO -- ARCH is $ARCH -- DISTRO_NICK is $DISTRO_NICK" "1"
 
 	REPO_PACKAGES_FILE="repopackages.php"
-	WGET_TMP_FILE="wget.tmp"
-	wget http://yum.postgresql.org/${REPO_PACKAGES_FILE} -o $WGET_TMP_FILE
+	BASE_URL="http://yum.postgresql.org/"
+	WGET_TMP_FILE="${WORKSPACE_WD}/data/wget.tmp"
 	
-	if [[ -f "$REPO_PACKAGES_FILE" ]]; then
+	wget --quiet ${BASE_URL}${REPO_PACKAGES_FILE} -O ${WORKSPACE_WD}/data/$REPO_PACKAGES_FILE > $WGET_TMP_FILE
+	
+	if [[ -f "${WORKSPACE_WD}/data/$REPO_PACKAGES_FILE" ]]; then
 		choosePostgresVersion			#sets VERSION
 	
 		MAJOR_VERSION=${VERSION:0:1}
 		MINOR_VERSION=${VERSION:1:2}
 		
-		resetVariableInFile "POSTGRES_MAJOR_VERSION" "${MAJOR_VERSION}"
-		resetVariableInFile "POSTGRES_MINOR_VERSION" "${MINOR_VERSION}"
+		resetVariableInVariableFile "POSTGRES_MAJOR_VERSION" "${MAJOR_VERSION}"
+		resetVariableInVariableFile "POSTGRES_MINOR_VERSION" "${MINOR_VERSION}"
 		
 		DOT_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}"
 		
@@ -120,7 +127,7 @@ function getPostgresRepo () {
 		REPO_WO_RPM="/${DOT_VERSION}/${DISTRO}/${DISTRO_NICK}-${DISTRO_VERSION}-${ARCH}/"
 		
 		outputLog "Link used to get build number: ${REPO_WO_RPM}${RPM}" "1"
-		LINK=`grep ${REPO_WO_RPM}${RPM} $REPO_PACKAGES_FILE`
+		LINK=`grep ${REPO_WO_RPM}${RPM} ${WORKSPACE_WD}/data/$REPO_PACKAGES_FILE`
 		outputLog "Link from file is $LINK" "1"
 		INDEX=`awk -v a="$LINK" -v b=".noarch" 'BEGIN{print index(a,b)}'`
 		outputLog "Index found at $INDEX" "1"
@@ -158,16 +165,17 @@ function installPostgres () {
 
 	outputLog "Getting postgreSQL pgdg RPM from $REPO\n" "2"
 		
-	curl -O $REPO
+	curl -0 $REPO -o ${WORKSPACE_WD}/data/$RPM 2>/dev/null
 	
-	rpm -ivh $RPM
+	rpm -ivh ${WORKSPACE_WD}/data/$RPM
 	newLine
 	
 	BUILD_VERSION=
 	MAINTENANCE_VERSION=
 	
 	IFS=$'\n'
-	POSTGRESQL_RPM_ARRAY=( $(yum provides postgresql${VERSION} | grep "postgresql${VERSION}") )
+	#Find all the minor/maintanence build numbers, excluding the beta and rc versions
+	POSTGRESQL_RPM_ARRAY=( $(yum provides postgresql${VERSION} | grep "postgresql${VERSION}" | grep -v "beta" | grep -v "rc") )
 	findLatestPgRpmVersion "${POSTGRESQL_RPM_ARRAY[@]}"
 	newLine
 	
@@ -189,41 +197,50 @@ function installPostgres () {
 	yum -y install $POSTGRESQL_LIB_RPM
 	yum -y install $POSTGRESQL_RPM $POSTGRESQL_SERVER_RPM
 	
-	resetVariableInFile "POSTGRES_SERVICE_NAME" "postgresql-${MAJOR_VERSION}.${MINOR_VERSION}"
+	resetVariableInVariableFile "POSTGRES_SERVICE_NAME" "postgresql-${MAJOR_VERSION}.${MINOR_VERSION}"
 	loadVariables
 	newLine
 
-	outputLog "Looking for $POSTGRES_SERVICE_FILE" "1"
-	if [ -f $POSTGRES_SERVICE_FILE ]; then
-
-		newLine
-		service $POSTGRES_SERVICE_NAME initdb
+	#Big switch in postgres install from version 9.1-4
+	if [[ "${MAJOR_VERSION}" -le "9" && "${MINOR_VERSION}" -le "1" && "$BUILD_VERSION" -le "4" ]]; then
 		
-		outputLog "Copying postgres config file to $POSTGRES_INSTALL_LOCATION/${MAJOR_VERSION}.${MINOR_VERSION}/data/pg_hba.conf" "2"
-		cp ${WORKSPACE_WD}/conf/postgres/pg_hba.conf $POSTGRES_INSTALL_LOCATION/${MAJOR_VERSION}.${MINOR_VERSION}/data/pg_hba.conf
-
-		outputLog "Setting $POSTGRES_SERVICE_NAME to default to on after a machine restart..." "2"
-		chkconfig $POSTGRES_SERVICE_NAME on
+		outputLog "Looking for $POSTGRES_SERVICE_FILE" "1"
+		if [ -f $POSTGRES_SERVICE_FILE ]; then
+	
+			newLine
+			service $POSTGRES_SERVICE_NAME initdb
+			
+			outputLog "Copying postgres config file to $POSTGRES_INSTALL_LOCATION/${MAJOR_VERSION}.${MINOR_VERSION}/data/pg_hba.conf" "2"
+			cp ${WORKSPACE_WD}/conf/postgres/pg_hba.conf $POSTGRES_INSTALL_LOCATION/${MAJOR_VERSION}.${MINOR_VERSION}/data/pg_hba.conf
+	
+			outputLog "Setting $POSTGRES_SERVICE_NAME to default to on after a machine restart..." "2"
+			chkconfig $POSTGRES_SERVICE_NAME on
+			
+			newLine
+			startPostgresService
+			resetVariableInVariableFile "POSTGRES_INSTALLED" "y"
+			
+		else
+			outputLog "Postgres has not been installed, yum installation failed, check output above." "4"
+		fi	
+	else
+		#Handling install of versions of postgres newer then 9.1-4
+		eval su - postgres -c "/usr/pgsql-${MAJOR_VERSION}.${MINOR_VERSION}/bin/initdb"
 		
+		systemctl enable ${POSTGRES_SERVICE_NAME}.service
+			
 		newLine
 		startPostgresService
-		resetVariableInFile "POSTGRES_INSTALLED" "y"
-	#elif [ -f "/lib/systemd/system/postgresql-${MAJOR_VERSION}.${MINOR_VERSION}.service" ]; then
-	#	/usr/pgsql-${MAJOR_VERSION}.${MINOR_VERSION}/bin/postgresql${MAJOR_VERSION}${MINOR_VERSION}-setup initdb
-		
-		
-	else
-		outputLog "Postgres has not been installed, yum installation failed, check output above." "4"
-	fi	
-
+		resetVariableInVariableFile "POSTGRES_INSTALLED" "y"
+	fi
 }
 
 #function - deletePostgresTmpFiles () - will delete all temp files created in the process of install postgresql
 function deletePostgresTmpFiles () {	
 	deleteFile $WGET_TMP_FILE
-	deleteFile $REPO_PACKAGES_FILE
-	deleteFile $RPM	
-	deleteFile list.txt
+	deleteFile ${WORKSPACE_WD}/data/$REPO_PACKAGES_FILE
+	deleteFile ${WORKSPACE_WD}/data/$RPM	
+	deleteFile ${WORKSPACE_WD}/data/list.txt
 }
 
 #function - findLatestPgRpmVersion (rpmArray) - find the version with the highest minor/build numbers and choose that
@@ -243,6 +260,7 @@ function findLatestPgRpmVersion () {
 		if [[ "$V" =~ "Provides-match" ]]; then
 			outputLog "Ignoring last line in array..." "1"
 		else
+
 			BUILD_VERSION=${V:17:1}
 			MAINTENANCE_VERSION=${V:19:1}
 			outputLog "BUILD_VERSION [$BUILD_VERSION] -- MAINTENANCE_VERSION [$MAINTENANCE_VERSION]" "1"
@@ -260,12 +278,7 @@ function findLatestPgRpmVersion () {
 			elif [[ $TOP_BUILD_NUMBER -eq $BUILD_VERSION && $TOP_MAINTENANCE_NUMBER -lt $MAINTENANCE_VERSION ]]; then
 				TOP_MAINTENANCE_NUMBER=$MAINTENANCE_VERSION
 				TOP_CHOICE=$COUNT
-				
-				#TODO hardcode 4.1 as default
-				if [[ "$BUILD_VERSION" == "4" && "$MAINTENANCE_VERSION" == "1" ]]; then
-					TEMP_CHOICE=$COUNT
-				fi
-				
+								
 				outputLog "Build numbers are the same [$BUILD_VERSION], but current maintence greater then $TOP_MAINTENANCE_NUMBER, set TOP_MAINTENANCE_NUMBER[$TOP_MAINTENANCE_NUMBER]" "1"
 			fi
 		fi
@@ -273,19 +286,15 @@ function findLatestPgRpmVersion () {
 		outputLog "\t${COUNT}. postgresql${VERSION}.${BUILD_VERSION}-${MAINTENANCE_VERSION}" "2" "y" "n"
 	done
 	
-	#TODO hardcode 4.1 as default
-	TOP_CHOICE=$TEMP_CHOICE
-	TOP_MAINTENANCE_NUMBER=1
-	TOP_BUILD_NUMBER=4
-	
 	while true;
 	do
-		takeInput "Choose the build/maintenance version you desire, it's recommended to use the default: (${TOP_BUILD_NUMBER}-${TOP_MAINTENANCE_NUMBER})"
+		takeInput "Choose the build/maintenance version you desire, it's recommended to use the default for 9.1: (${TOP_BUILD_NUMBER}-${TOP_MAINTENANCE_NUMBER})"
 		read DETAIL_VERSION_TO_INSTALL
 		
 		#If non-numeric or not in the correct number range, then invalid else extract version to add to repo base
 		if [[ "$DETAIL_VERSION_TO_INSTALL" == "b" || "$DETAIL_VERSION_TO_INSTALL" == "B" ]]; then
 			deletePostgresDB "$POSTGRES_JON_DB"
+			deletePostgresTmpFiles
 			resetVariableInVariableFile "POSTGRES_JON_DB"
 			INSTALL_BUNDLES=""
 			loadVariables
@@ -310,10 +319,6 @@ function findLatestPgRpmVersion () {
 	
 	outputLog "TOP_BUILD_NUMBER: [$TOP_BUILD_NUMBER] -- TOP_MAINTENANCE_NUMBER: [$TOP_MAINTENANCE_NUMBER]"
 	
-	#TODO HARDCODED -- fix 6-1 update!!
-	TOP_BUILD_NUMBER="1"
-	TOP_MAINTENANCE_NUMBER="4"
-	
 	
 }
 
@@ -321,10 +326,11 @@ function findLatestPgRpmVersion () {
 function choosePostgresVersion () {
 	
 	#Truncate the file to only the available repos
-	sed '/Available Repository RPMs/, /EOL/ !d' repopackages.php > list.txt
+	local LIST_FILE="${WORKSPACE_WD}/data/list.txt"
+	sed '/Available Repository RPMs/, /EOL/ !d' ${WORKSPACE_WD}/data/repopackages.php > $LIST_FILE
 	
 	#Get the list of a tags containing the postgres version numbers
-	TEMP=`grep "<a name=" list.txt`
+	TEMP=`grep "<a name=" $LIST_FILE`
 	#outputLog "TEMP list is: $TEMP" "1"
 	
 	VERSION_ARRAY=()
@@ -336,11 +342,11 @@ function choosePostgresVersion () {
 		if [[ "$LINE" =~ "font color=red" || "$LINE" =~ "BETA" ]]; then 
 			BETA_VERSION=$COUNT
 		fi
-		
+
 		#outputLog "Beta version matches with index $BETA_VERSION of postgreSQL versions"
 		
-		T1=${LINE#*\"}
-		T1=${T1%\"*}
+		T1=${LINE#*\"} ###pg90fedora"></a>
+		T1=${T1%\"*}   ###pg90fedora
 		#outputLog "T1 is: $T1" "1"
 		
 		T1_LENGTH=${#T1}
@@ -350,23 +356,39 @@ function choosePostgresVersion () {
 			T1=${T1:2:2}
 		fi
 		
-		outputLog "Checking for \"/${T1:0:1}.${T1:1:1}/${DISTRO}/${DISTRO_NICK}-${DISTRO_VERSION}-${ARCH}/\" in list.txt"
-		DISTRO_PG_OK=`grep "/${T1:0:1}.${T1:1:1}/${DISTRO}/${DISTRO_NICK}-${DISTRO_VERSION}-${ARCH}/" list.txt`
-		
+		outputLog "Checking for \"/${T1:0:1}.${T1:1:1}/${DISTRO}/${DISTRO_NICK}-${DISTRO_VERSION}-${ARCH}/\" in $LIST_FILE"
+		DISTRO_PG_OK=`grep "/${T1:0:1}.${T1:1:1}/${DISTRO}/${DISTRO_NICK}-${DISTRO_VERSION}-${ARCH}/" $LIST_FILE`
+		outputLog "distro <-- $DISTRO_PG_OK"
 		if [[ "$DISTRO_PG_OK" == "" ]]; then
 			outputLog "Skipping PostgreSQL v.${T1} as it's not available for the current distro" "1"
 		else 
-		
-			#outputLog "T1 is: $T1" "1"
-			#outputLog "v array is: ${VERSION_ARRAY[@]}" "1"
+			#Check if the URL is valid before adding it
+			local LINK=""
+			LINK=${DISTRO_PG_OK#*\"}   #Text after first double quote
+			LINK=${LINK%%\"*}   #Text after first double quote, full relative link
 			
-			MATCH=$(echo "${VERSION_ARRAY[@]}" | grep -o $T1)  
-			if [[ "$MATCH" == "" ]]; then
-			  outputLog "Adding $T1 to the array of PostgreSQL versions" "1"
-			  VERSION_ARRAY+=($T1)
-			  COUNT=$((COUNT + 1))
-			else
-			  outputLog "Ignoring $T1 as it already exists in the array" "1"
+			#Check if the URL is valid
+			local RESULT=`curl -0 ${BASE_URL}${LINK} 2>/dev/null`
+			if [[ "$RESULT" =~ "404" ]]; then
+				outputLog "This version - ${T1} - is supposedly available for your architecture -throws a 404 on the PostgreSQL website, ignoring..." "3"
+			else	
+		
+				#outputLog "T1 is: $T1" "1"
+				#outputLog "v array is: ${VERSION_ARRAY[@]}" "1"
+
+				if [[ "$T1" -le "$LATEST_SUPPORTED_POSTGREQ" ]]; then
+				
+					MATCH=$(echo "${VERSION_ARRAY[@]}" | grep -o $T1)  
+					if [[ "$MATCH" == "" ]]; then
+					  outputLog "Adding $T1 to the array of PostgreSQL versions" "1"
+					  VERSION_ARRAY+=($T1)
+					  COUNT=$((COUNT + 1))
+					else
+					  outputLog "Ignoring $T1 as it already exists in the array" "1"
+					fi
+				else
+					outputLog "Ignoring PostgreSQL v${T1} as it is not supported by JON" "3"
+				fi
 			fi
 		fi
 		
@@ -375,47 +397,54 @@ function choosePostgresVersion () {
 
 	VERSION_ARRAY_LENGTH=$((${#VERSION_ARRAY[@]}))
 	outputLog "Array length is: $VERSION_ARRAY_LENGTH" "1"
+	
+	#If there are no versions add in the array, then we have not found a compatible PostgreSQL version
+	#And cannot install the JON demo in this environment
+	if [[ "$VERSION_ARRAY_LENGTH" == "0" ]]; then
+		outputLog "Sorry, there are no supported PostgreSQL versions for your architecture, try with one of the verified configurations found in the README-MORE file" "4"
+		exit
+	else
 
-	newLine "1"
-	takeInput "Select the version of postgres you would like to install: [1 ... x]\n\tB. Back to Main Menu.\n"
-
-	while true;
-	do
-		VERSION=
-
-		for (( i = 1 ; i <= $VERSION_ARRAY_LENGTH ; i++ ))
-		#i in $(seq $COUNT $VERSION_ARRAY_LENGTH)
-		do			
-			INDEX_FOR_ARRAY=$((i - 1))
-			
-			if [[ "$BETA_VERSION" == "$INDEX_FOR_ARRAY" ]]; then
-				BETA_TEXT=" BETA version"
-			else 
-				BETA_TEXT=""
+		newLine "1"
+		takeInput "Select the version of postgres you would like to install: [1 ... x]\n\tB. Back to Main Menu.\n"
+	
+		while true;
+		do
+			VERSION=
+	
+			for (( i = 1 ; i <= $VERSION_ARRAY_LENGTH ; i++ ))
+			#i in $(seq $COUNT $VERSION_ARRAY_LENGTH)
+			do			
+				INDEX_FOR_ARRAY=$((i - 1))
+				
+				if [[ "$BETA_VERSION" == "$INDEX_FOR_ARRAY" ]]; then
+					BETA_TEXT=" BETA version"
+				else 
+					BETA_TEXT=""
+				fi
+				takeInput "$i. -- PostgreSQL v.${VERSION_ARRAY[$INDEX_FOR_ARRAY]}${BETA_TEXT}" "0"
+			done
+	
+			read VERSION
+	
+			#If non-numeric or not in the correct number range, then invalid else extract version to add to repo base
+			if [[ "$VERSION" == "b" || "$VERSION" == "B" ]]; then
+				deletePostgresTmpFiles
+				mainMenu
+			elif [[ "$VERSION" != +([0-9]) || "$VERSION" -lt "1" || "$VERSION" -gt "$VERSION_ARRAY_LENGTH" ]]; then
+				outputLog "Invalid input, must be between 1 and $VERSION_ARRAY_LENGTH" "4"
+				newLine
+			else
+				#Decrement version by one to match the array indices
+				VERSION=$((VERSION - 1))
+				
+				#Get the correct PG version from the array
+				VERSION=${VERSION_ARRAY[$VERSION]}
+				break
 			fi
-			takeInput "$i. -- PostgreSQL v.${VERSION_ARRAY[$INDEX_FOR_ARRAY]}${BETA_TEXT}" "0"
+	
 		done
-
-		#echo -en "\t"
-		read VERSION
-
-		#If non-numeric or not in the correct number range, then invalid else extract version to add to repo base
-		if [[ "$VERSION" == "b" || "$VERSION" == "B" ]]; then
-			deletePostgresTmpFiles
-			mainMenu
-		elif [[ "$VERSION" != +([0-9]) || "$VERSION" -lt "1" || "$VERSION" -gt "$VERSION_ARRAY_LENGTH" ]]; then
-			outputLog "Invalid input, must be between 1 and $VERSION_ARRAY_LENGTH" "4"
-			newLine
-		else
-			#Decrement version by one to match the array indices
-			VERSION=$((VERSION - 1))
-			
-			#Get the correct PG version from the array
-			VERSION=${VERSION_ARRAY[$VERSION]}
-			break
-		fi
-
-	done
+	fi
 }
 
 function getDistroDetails () {
